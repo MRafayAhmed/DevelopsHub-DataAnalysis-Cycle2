fcc2fca8b98899c5c6c14ac1e761679ecacc4762
MRafayAhmed <rafaa2524@gmail.com>
2025-06-23 18:05:25 +0500

    Dataset Download

---
+++ Task01/IDMdatasetdown.py
@@ -0,0 +1,5 @@
+import kagglehub 
+path = kagglehub.dataset_download("pavansubhasht/ibm-hr-analytics-attrition-dataset")
+print("Path to dataset files", path)
+
+#print("C:\Users\Administrator\.cache\kagglehub\datasets\pavansubhasht\ibm-hr-analytics-attrition-dataset\versions\1:")
