bc033ffbb40375de86df3fd99b0028dc86c0e158
MRafayAhmed <rafaa2524@gmail.com>
2025-06-26 16:22:19 +0500

    finalcommit

--- Task02/CNNdatasetdown.py
+++ Task02/CNNdatasetdown.py
@@ -4,3 +4,5 @@ import kagglehub
 path = kagglehub.dataset_download("shubham3112/cnn-daily-mail-dataset")
 
 print("Path to dataset files:", path)
+
+#C:/Users/Administrator/.cache/kagglehub/datasets/shubham3112/cnn-daily-mail-dataset/versions/1/CNN_Daily_Mail_Dataset.csv
